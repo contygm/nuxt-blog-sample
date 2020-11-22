0b3cfa789634dbc07664d131cb0fd5aefdca2189
contygm <contygm@gmail.com>
2020-11-21 21:25:22 -0800

    blog basics

---
+++ components/posts/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+<section class="post-list">
+    <PostPreview id="1" thumbnail="https://miro.medium.com/max/12032/0*__5nhm_2qHSrTVoZ" title="Title 1" previewText="First Post" />
+    <PostPreview id="2" thumbnail="https://miro.medium.com/max/12032/0*__5nhm_2qHSrTVoZ" title="Title 2" previewText="Second Post" />
+    <PostPreview id="3" thumbnail="https://miro.medium.com/max/12032/0*__5nhm_2qHSrTVoZ" title="Title 3" previewText="Third Post" />
+</section>
+</template>
+
+<script>
+import PostPreview from "@/components/posts/PostPreview"
+export default {
+    components: {
+        PostPreview
+    }
+}
+</script>
+
+<style scoped>
+.post-list {
+    display: flex;
+    padding: 20px;
+    box-sizing: border-box;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false" />
+    <Nuxt />
+</div>
+</template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader';
+import TheSideNav from '@/components/Navigation/TheSideNav';
+
+export default {
+    components: {
+        TheHeader,
+        TheSideNav
+    },
+    data() {
+        return {
+            displaySidenav: false
+        }
+    },
+}
+</script>
+
+<style>
+html {
+    font-family: 'Open Sans', sans-serif
+}
+
+body {
+    margin: 0;
+}
+</style>
